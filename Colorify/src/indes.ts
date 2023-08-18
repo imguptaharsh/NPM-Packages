b97c25148f0fbf2888c80f1ec5a4a9cfcc81257b
Harsh Kumar Gupta <111433732+imguptaharsh@users.noreply.github.com>
2023-08-19 01:38:53 +0530

    add logger functionality

---
+++ Colorify/src/indes.ts
@@ -0,0 +1,13 @@
+export class Log{
+    
+    static success(msg:string){
+        console.log(`%c${msg}`, 'color: green; font-weight: bold')
+    }
+    static danger(msg:string){
+        console.log(`%c${msg}`, 'color: red; font-weight: bold')
+    }
+    static info(msg:string){
+        console.log(`%c${msg}`, 'color: black; font-weight: bold','background: yellow')
+    }
+        
+}
